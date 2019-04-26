a3c5954cbace41446cf6fdeb0a449cab36642663
Scott Sadler <scott@alphanode.co>
2019-04-26 03:03:54 -0300

    wip

--- src/Control/Pipe/Extras.hs
+++ src/Control/Pipe/Extras.hs
@@ -20,39 +20,42 @@ map f = awaitForever $ yield . f
 scanl :: Monad m => (a -> b -> a) -> a -> Pipe b a m ()
 scanl f =
   fix $ \next s ->
-    let go i = let s' = f s i in yield s >> next s
+    let go i = yield s >> next (f s i)
      in await >>= maybe (yield s) go
 
 -- | Consume all input and return a list
 sinkList :: Monad m => Pipe i Void m [i]
-sinkList = await >>= maybe (pure []) (\i -> (i:) <$> sinkList)
+sinkList = foldl (flip (:)) []
 {-# INLINE sinkList #-}
 
 -- | Consume all values using given function
 awaitForever :: Monad m => (i -> Pipe i o m ()) -> Pipe i o m ()
-awaitForever f = Pipe $ flip $
+awaitForever f = Pipe $
   \rest ->
     fix $
-      \next (LR await yield) -> 
+      \next await yield -> 
         runAwait' await $
           \await' ->
-            let lr = LR await' yield
-                run i = unPipe (f i) lr $ \lr' () -> next lr'
-             in maybe (rest lr ()) run
+            let run i = unPipe (f i) (\l r () -> next l r) await' yield
+             in maybe (rest await' yield ()) run
 {-# INLINE awaitForever #-}
 
 foldl :: Monad m => (a -> i -> a) -> a -> Pipe i Void m a
 foldl f start = Pipe $
-  \lr rest ->
-    let next (LR l r) s =
+  \rest ->
+    let next s l r =
           runAwait' l $ \await' ->
-            let lr' = LR await' r
-             in maybe (rest lr' s) (next lr' . f s)
-     in next lr start
+            maybe (rest await' r s) (\i -> next (f s i) await' r)
+     in next start
 {-# INLINE foldl #-}
 
 sinkNull :: Monad m => Pipe i Void m ()
-sinkNull = awaitForever $ \_ -> pure ()
+sinkNull = Pipe $
+  \rest ->
+    fix $
+      \f l r ->
+        runAwait' l $
+          \l -> maybe (rest l r ()) $ \_ -> f l r
 
 awaitJust :: Monad m => (i -> Pipe i o m ()) -> Pipe i o m ()
 awaitJust f = await >>= maybe (pure ()) f
@@ -63,7 +66,9 @@ dropWhile_ f =
 {-# INLINE dropWhile_ #-}
 
 takeWhile_ :: Monad m => (i -> Bool) -> Pipe i i m ()
-takeWhile_ f = awaitJust (\i -> if f i then yield i else pure ())
+takeWhile_ f =
+  awaitJust (\i -> if f i then yield i >> takeWhile_ f else pure ())
+{-# INLINE takeWhile_ #-}
 
 identity_ :: Monad m => Pipe i i m ()
 identity_ = awaitForever yield
@@ -87,23 +92,23 @@ filter_ f = awaitForever $ \i -> if f i then yield i else pure ()
 
 zip_ :: Monad m => Pipe () o m () -> Pipe () o m () -> Pipe () (o, o) m ()
 zip_ (Pipe f1) (Pipe f2) = Pipe $
-  \(LR _ r) rest -> do
+  \rest _ r -> do
 
     let loop' (Just i1) a1 (Just i2) a2 yield = do
           runYield yield (Just (i1, i2)) $ \yield' -> do
             runAwait a1 $ \i1' a1' -> do
               runAwait a2 $ \i2' a2' -> do
                 loop' i1' a1' i2' a2' yield'
-        loop' _ _ _ _ yield = rest (LR mempty yield) ()
+        loop' _ _ _ _ yield = rest mempty yield ()
 
         y1 = Yield $ \mi1 a1 ->
               let y2 = Yield $ \mi2 a2 ->
                     loop' mi1 a1 mi2 a2 r
-               in f2 (LR mempty y2) (\(LR _ r) () -> terminate r)
+               in f2 (\_ r () -> terminate r) mempty y2
 
         terminate y = runYield y Nothing terminate
 
-     in f1 (LR mempty y1) (\(LR _ r) () -> terminate r)
+     in f1 (\_ r () -> terminate r) mempty y1
 
 
 concat_ :: Monad m => Pipe [i] i m ()
--- src/Control/Pipe/Internal.hs
+++ src/Control/Pipe/Internal.hs
@@ -11,12 +11,10 @@ module Control.Pipe.Internal
   , Await(..)
   , runAwait
   , runAwait'
-  , LR(..)
   , Yield(..)
   , runYield
   , Await'
   , Yield'
-  , provide
   ) where
 
 
@@ -40,29 +38,30 @@ runAwait :: Await i a -> Yield' i a -> a
 runAwait (Await a) = a . Yield
 {-# INLINE runAwait #-}
 
+runAwait' :: Await i a -> (Await i a -> Maybe i -> a) -> a
 runAwait' (Await a) = a . Yield . flip
 
 runYield :: Yield i a -> Maybe i -> Await' i a -> a
 runYield (Yield a) i = a i . Await
 {-# INLINE runYield #-}
 
-provide :: [i] -> Await i a
-provide [] = mempty
-provide (x:xs) = Await $ \yield -> unYield yield (Just x) $ provide xs
-
 instance Semigroup (Await i a) where
   a1 <> a2 =
-    Await $ \yield ->
-      let out Nothing _ = unAwait a2 yield
-          out mi      a = unYield yield mi $ a <> a2
+    Await $ \yld ->
+      let out Nothing _ = unAwait a2 yld
+          out mi      a = unYield yld mi $ a <> a2
        in runAwait a1 out
 
 instance Monoid (Await i a) where
-  mempty = Await $ \yield -> unYield yield Nothing mempty
+  mempty = Await $ \yld -> unYield yld Nothing mempty
 
 --
 -- Monad
 --
+
+-- | Left and right continuations
+type L i m r = Await i (m r)
+type R o m r = Yield o (m r)
 
 -- | Pipe datatype. 
 --
@@ -75,25 +74,22 @@ instance Monoid (Await i a) where
 --   a = return type
 --
 newtype Pipe i o m a =
-  Pipe { unPipe :: forall r. LR i o m r -> (LR i o m r -> a -> m r) -> m r }
-
--- | Left and right continuations
-data LR i o m r = LR (Await i (m r)) (Yield o (m r))
+  Pipe { unPipe :: forall r. (L i m r -> R o m r -> a -> m r) -> L i m r -> R o m r -> m r }
 
 instance Monad m => Functor (Pipe i o m) where
-  fmap f (Pipe c) = Pipe $ \r rest -> c r (\r -> rest r . f)
+  fmap f (Pipe p) = Pipe $ \rest l r -> p (\l r -> rest l r . f) l r
 
 instance Monad m => Applicative (Pipe i o m) where
   pure = return
   (<*>) = ap
 
 instance Monad m => Monad (Pipe i o m) where
-  return x = Pipe $ \r f -> f r x
+  return x = Pipe $ \f l r -> f l r x
   Pipe f >>= g = Pipe $
-    \r h -> f r $ \r' a -> unPipe (g a) r' h
+    \rest -> f (\l r a -> unPipe (g a) rest l r)
 
 instance MonadTrans (Pipe i o) where
-  lift mf = Pipe $ \r f -> mf >>= f r
+  lift mf = Pipe $ \f l r -> mf >>= f l r
 
 instance MonadIO m => MonadIO (Pipe i o m) where
   liftIO = lift . liftIO
@@ -116,7 +112,7 @@ instance Monad m => Monoid (Pipe i o m ()) where
 --
 await :: Pipe i o m (Maybe i)
 await = Pipe $
-  \(LR a y) f -> runAwait a $ \i a' -> f (LR a' y) i
+  \rest a y -> runAwait a $ \i a' -> rest a' y i
 {-# INLINE await #-}
 
 
@@ -124,27 +120,27 @@ await = Pipe $
 --
 yield :: o -> Pipe i o m ()
 yield i = Pipe $
-  \(LR a y) f -> runYield y (Just i) $ \y' -> f (LR a y') ()
+  \rest a y -> runYield y (Just i) $ \y' -> rest a y' ()
 {-# INLINE yield #-}
 
 
 -- | Run pipe to completion.
 --
 runPipe :: Monad m => Pipe () Void m r -> m r
-runPipe pipe = unPipe pipe (LR mempty voidOut) (\_ -> pure)
+runPipe pipe = unPipe pipe (\_ _ -> pure) mempty voidOut
   where
     voidOut = Yield $ \(Just i) _ -> absurd i
 
-
 -- | Compose a pipe with another pipe; the return type is that of the second pipe.
 --
-(.|) :: Applicative m => Pipe i e m () -> Pipe e o m b -> Pipe i o m b
+(.|) :: forall i o e m b. Pipe i e m () -> Pipe e o m b -> Pipe i o m b
 (Pipe f1) .| (Pipe f2) =
-  Pipe $ \(LR await yield) rest -> do
-    let rest' (LR _ y) = rest $ LR mempty y
-        one await = maybe await (\i -> provide [i] <> await)
-        right mi await = f2 (LR (one await mi) yield) rest'
+  Pipe $ \rest l r -> do
+    let rest' _ y = rest mempty y
+        one (Just i) left = Await $ \y -> unYield y (Just i) left
+        one Nothing left = left
+        right mi left = f2 rest' (one mi left) r
         terminate y = runYield y Nothing terminate
     -- The yield right function triggers the right hand pipe
     -- The return method terminates to the right
-    f1 (LR await (Yield right)) (\(LR _ y) () -> terminate y)
+    f1 (\_ y () -> terminate y) l (Yield right)
